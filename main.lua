827d6c40fd328327c25175e8d00122110101109d
jashinnoko <yamaken0232@gmail.com>
2026-03-26 11:34:23 +0900

    Notify success on loading GitHub script
    Replaced print statement with notification to StarterGui.

--- main.lua
+++ main.lua
@@ -1,1 +1,9 @@
-print("GitHubからの読み込みに成功しました！")
+local StarterGui = game:GetService("StarterGui")
+
+-- 画面に「成功」と通知を出すコード
+StarterGui:SetCore("SendNotification", {
+    Title = "読み込み成功！",
+    Text = "GitHubのスクリプトが動いています",
+    Duration = 10
+})
+
